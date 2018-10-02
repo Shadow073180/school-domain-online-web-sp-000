1aa17118d342ca6cdc4b82c54b80ed01033da4b7
David masterson <david73180@gmail.com>
2018-10-02 20:40:12 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -43,9 +43,10 @@ class School
     # to an empty array as the value of the key value pair.
     # then pushes the student name to that array.
     def add_student(name, grade)
+      binding.pry
       @roster[grade] ||= []
       @roster[grade].push(name)
-      binding.pry
+      
     end
 
 end
